a0430188cc56d71bcd8398a4cc291ff6c5e03a4a
Heberth Junor <hjunor@gmail.com>
2021-03-01 16:46:52 -0300

    refactor: server unic module starter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import express, { urlencoded }  from 'express';
-import 'reflect-metadata';
-import './database';
-import { router } from './routes/links.routes';
+import { app } from "./app";
 
 const port = 3002;
 
-const app = express();
-
-app.use(express.json());
-
-app.use(urlencoded({extended:false}));
-
-app.use(router)
 
 app.listen(port, ()=>{
   console.log('server start 🔥 ', port)
